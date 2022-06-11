b7ed1f02acae58c8316051f46b4d3613f22ac3b3
myunghan <myunghan@myunghanui-iMac.local>
2022-06-11 14:14:31 +0900

    잡파라미터에 접근하기

--- src/main/java/io/spring/batch/helloworld/HelloWorldApplication.java
+++ src/main/java/io/spring/batch/helloworld/HelloWorldApplication.java
@@ -6,10 +6,12 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -42,16 +44,60 @@ public class HelloWorldApplication {
 				}).build();
 	}
 
+	@Bean
+	public Step step1() {
+		return this.stepBuilderFactory.get("step1")
+				.tasklet(((stepContribution, chunkContext) -> {
+					System.out.println("Hello, World!");
+					return RepeatStatus.FINISHED;
+				})).build();
+	}
+
+	@Bean
+	public Step step2() {
+		return this.stepBuilderFactory.get("step2")
+				.tasklet(helloWorldTasklet2(null))
+				.build();
+	}
+
+	/**
+	 * spring 구성을 사용해 JobParameters에 접근하기
+	 */
+	@Bean
+	public Tasklet helloWorldTasklet1() {
+		return ((stepContribution, chunkContext) -> {
+			String name = (String) chunkContext.getStepContext()
+					.getJobParameters()
+					.get("name");
+
+			System.out.println(String.format("Hello, %s", name));
+			return RepeatStatus.FINISHED;
+		});
+	}
+
+	/**
+	 * 늦은 바인딩을 통해 JobParameters 접근하기
+	 */
+	@StepScope
+	@Bean
+	public Tasklet helloWorldTasklet2(@Value("#{jobParameters['name']}") String name) {
+		return ((stepContribution, chunkContext) -> {
+			System.out.println(String.format("Hello, %s!", name));
+			return RepeatStatus.FINISHED;
+		});
+	}
+
+
 	/**
 	 * job 만들기
 	 */
 	@Bean
 	public Job job() {
-		return this.jobBuilderFactory.get("job")
+		return this.jobBuilderFactory.get("basicJob")
 				/**
 				 * 등록해둔 step 장착
 				 */
-				.start(step())
+				.start(step2())
 				.build();
 	}
 
